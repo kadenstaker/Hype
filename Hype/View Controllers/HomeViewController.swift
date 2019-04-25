5fb03d0e7a19de041992feddbd38ed1b8ea9a3f3
Kaden <kadenstaker@gmail.com>
2019-04-25 14:41:47 -0600

    adds basic ui

---
+++ Hype/View Controllers/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  Hype
+//
+//  Created by Kaden Staker on 4/25/19.
+//  Copyright © 2019 Kaden Staker. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+ 
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+
+}
