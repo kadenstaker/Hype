488378a77b8239af0429a837c41c818fe7fb62e3
Kaden <kadenstaker@gmail.com>
2019-04-29 15:02:41 -0600

    more UI changes and functionality

---
+++ Hype/View Controllers/ChallengeTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ChallengeTableViewCell.swift
+//  Hype
+//
+//  Created by Kaden Staker on 4/29/19.
+//  Copyright © 2019 Kaden Staker. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var challengeLabel: UILabel!
+    @IBOutlet weak var challengeButton: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
