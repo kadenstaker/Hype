6247abda77e62111877c354fbb9eed4aee3b15cc
Hannah <hannah@hofffamily.org>
2019-05-09 14:49:04 -0600

    energy graph all set up and working

--- Hype/View Controllers/Habits/EnergyViewController.swift
+++ Hype/View Controllers/Habits/EnergyViewController.swift
@@ -19,73 +19,52 @@ class EnergyViewController: UIViewController {
     var amount = [String]()
     
     var dataEntry: [BarChartDataEntry] = []
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateGraph()
         
-//        let xArray = Array(1..<10)
-//        let ys1 = xArray.map { x in return sin(Double(x) / 2.0 / 3.141 * 1.5) }
-//
-//        let yse1 = ys1.enumerated().map { x, y in return BarChartDataEntry(x: Double(x), y: y) }
-//
-//        let data = BarChartData()
-//        let ds1 = BarChartDataSet(entries: yse1, label: "KWhs")
-//        ds1.colors = [NSUIColor(red: 0.19, green: 0.69, blue: 1, alpha: 1)]
-//        data.addDataSet(ds1)
-//
-//        let barWidth = 0.4
-//        let barSpace = 0.05
-//        let groupSpace = 0.1
-//
-//        data.barWidth = barWidth
-//        self.barChartView.xAxis.axisMinimum = Double(xArray[0])
-//        self.barChartView.xAxis.axisMaximum = Double(xArray[0]) + data.groupWidth(groupSpace: groupSpace, barSpace: barSpace) * Double(xArray.count)
-//        // (0.4 + 0.05) * 2 (data set count) + 0.1 = 1
-//        data.groupBars(fromX: Double(xArray[0]), groupSpace: groupSpace, barSpace: barSpace)
-//
-//        self.barChartView.data = data
-//
-//        self.barChartView.gridBackgroundColor = NSUIColor.white
-//
-//        self.barChartView.chartDescription?.text = "Monthly Usage"
-        
-        setBarChart(dataPoints: amount, values: date)
     }
     
-    func setBarChart(dataPoints: [String], values: [String]) {
-        barChartView.noDataTextColor = UIColor.black
-        barChartView.noDataText = "No data for the chart."
-        barChartView.backgroundColor = UIColor.white
-        
-        for i in 0..<dataPoints.count {
-            let dataPoint = BarChartDataEntry(x: Double(i), y: Double(values[i])!)
-            print(dataPoint)
-            dataEntry.append(dataPoint)
-        }
-        
-                let chartDataSet = BarChartDataSet(entries: dataEntry, label: "gallons")
-                let chartData = BarChartData()
-                chartData.addDataSet(chartDataSet)
-                chartData.setDrawValues(false)
-                chartDataSet.colors = [UIColor(red: 0.19, green: 0.69, blue: 1, alpha: 1)]
-        
-                let formatter: ChartFormatter = ChartFormatter()
-                formatter.setValues(values: dataPoints)
-                let xaxis: XAxis = XAxis()
-                xaxis.valueFormatter = formatter
-                barChartView.xAxis.labelPosition = .bottom
-                barChartView.xAxis.drawGridLinesEnabled = false
-                barChartView.xAxis.valueFormatter = xaxis.valueFormatter
-                barChartView.chartDescription?.enabled = false
-                barChartView.legend.enabled = true
-                barChartView.rightAxis.enabled = false
-                barChartView.leftAxis.drawGridLinesEnabled = true
-                barChartView.leftAxis.drawLabelsEnabled = true
-                barChartView.data = chartData
-        
-            }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateGraph()
+    }
     
+    @IBAction func unwindWithData(_ sender: UIStoryboardSegue) {
+        if let viewController = sender.source as? EnergyBillViewController {
+            usage.append(viewController.input)
+        }
+    }
+    
+    func updateGraph() {
+        guard let barChartView = barChartView else { return }
+        
+        // this array that will eventually be displayed on the graph
+        var barChartEntry = [BarChartDataEntry]()
+        
+         //for loop over every element in the array usage
+        for i in 0..<usage.count {
+            // set the x and y status in a data chart entry
+            let value = BarChartDataEntry(x: Double(i), y: usage[i])
+            // add it to the data set
+            barChartEntry.append(value)
+        }
+        // convert barChartEntry to a BarChartDataSet
+        let line1 = BarChartDataSet(entries: barChartEntry, label: "Date")
+        
+        // sets the color to blue
+        line1.colors = [UIColor(red: 0.19, green: 0.69, blue: 1, alpha: 1)]
+        // this is the object that will be added to the chart
+        let data = BarChartData()
+
+        //adds the line to the dataSet
+        data.addDataSet(line1)
+        // adds the chart data to the chart and causes an update
+        barChartView.data = data
+        // sets the title for the graph
+        barChartView.chartDescription?.text = "Monthly Usage"
+    }
 }
 
     
